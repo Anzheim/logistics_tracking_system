ef80e062cd369aee760c7fe8b64f754f29b43120
Anzheim <77605411+Anzheim@users.noreply.github.com>
2023-12-17 21:54:51 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+$dsn = "";
+$user = "";
+$password = "";
+
+try {
+    $pdo = new PDO($dsn, $user, $password);
+    //echo "PDO connection established.";
+    echo "<br>";
+    return $pdo;
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    echo "PDO error code: " . $e->getCode();
+}
+
+?>
